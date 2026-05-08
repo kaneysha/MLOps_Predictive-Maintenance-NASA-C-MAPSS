8118aa80f8bec1d7faf8420f8e48f265c674c359
kaneysha <162722736+kaneysha@users.noreply.github.com>
2026-05-08 13:59:00 +0000

    nyoba lagi test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,14 +1,14 @@
-from src.preprocess import preprocess_data  # sesuaikan path kamu
+from src.preprocess import preprocess
 import pandas as pd
 
 def test_preprocessing_output_shape():
     df = pd.read_csv("data/raw/train.csv")
-    X = preprocess_data(df)
+    X = preprocess(df)
 
     assert X.shape[0] > 0
 
 def test_preprocessing_no_nan():
     df = pd.read_csv("data/raw/train.csv")
-    X = preprocess_data(df)
+    X = preprocess(df)
 
     assert not X.isnull().any().any()
